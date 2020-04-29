430d35adb94b52bf72894b48426756b707a63faa
Legrand Jérémie <jeremielegrand021@gmail.com>
2020-04-29 23:47:17 +0200

    feat: Creating both controller the first is for home and the second is for the properties.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
-class HomeController
+class HomeController extends  AbstractController
 {
+
+
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
     public function  index() :Response
     {
-        return  new Response('hello world');
+        return  $this->render('pages/home.html.twig');
     }
 }
