ee6bb6b147582bd693708712942528722b235154
Legrand Jérémie <jeremielegrand021@gmail.com>
2020-04-30 23:44:51 +0200

    feat: Frontend of index and property pages . Database created . adding one table named property with different parameters.Using doctrine.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,13 +14,14 @@ class HomeController extends  AbstractController
 
     /**
      * @Route("/", name="home")
+     * @param PropertyRepository $repository
      * @return Response
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
      */
-    public function  index() :Response
+    public function  index(PropertyRepository $repository): Response
     {
-        return  $this->render('pages/home.html.twig');
+        $properties = $repository->findLatest();
+        return  $this->render('pages/home.html.twig', [
+            'properties' => $properties
+        ]);
     }
 }
